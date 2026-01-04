8f1e22315fa751ed5101b746bb68b447be932b9d
Ashan Kasthuri <k.a.a.dilantha@gmail.com>
2026-01-04 20:08:26 +1300

    fix(binding): replace Napi::External with mutable Napi::Object to fix runtime crash in Node 25

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -5,15 +5,20 @@ typedef struct TSLanguage TSLanguage;
 extern "C" TSLanguage *tree_sitter_gosu();
 
 // "tree-sitter", "language" hashed with BLAKE2
-const napi_type_tag LANGUAGE_TYPE_TAG = {
-    0x8AF2E5212AD58ABF, 0xD5006CAD83ABBA16
-};
+const napi_type_tag LANGUAGE_TYPE_TAG = {0x8AF2E5212AD58ABF,
+                                         0xD5006CAD83ABBA16};
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_gosu());
-    language.TypeTag(&LANGUAGE_TYPE_TAG);
-    exports["language"] = language;
-    return exports;
+  auto language = Napi::Object::New(env);
+  language.TypeTag(&LANGUAGE_TYPE_TAG);
+  napi_status status =
+      napi_wrap(env, language, tree_sitter_gosu(), nullptr, nullptr, nullptr);
+  if (status != napi_ok) {
+    Napi::Error::New(env, "Failed to wrap language")
+        .ThrowAsJavaScriptException();
+  }
+  exports["language"] = language;
+  return exports;
 }
 
 NODE_API_MODULE(tree_sitter_gosu_binding, Init)
