fa4df37e3efcbe18a9fab1704e36a6235dc23685
Ashan Kasthuri <k.a.a.dilantha@gmail.com>
2026-01-05 04:23:16 +1300

    fix(binding): revert to Napi::External to fix type mismatch with native runtime

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -9,14 +9,8 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {0x8AF2E5212AD58ABF,
                                          0xD5006CAD83ABBA16};
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  auto language = Napi::Object::New(env);
+  auto language = Napi::External<TSLanguage>::New(env, tree_sitter_gosu());
   language.TypeTag(&LANGUAGE_TYPE_TAG);
-  napi_status status =
-      napi_wrap(env, language, tree_sitter_gosu(), nullptr, nullptr, nullptr);
-  if (status != napi_ok) {
-    Napi::Error::New(env, "Failed to wrap language")
-        .ThrowAsJavaScriptException();
-  }
   exports["language"] = language;
   return exports;
 }
